cd0f154bfb743c29c90c55c991f38fe6c90b96c5
Andrew Louis <andrew@hyfen.net>
2022-03-02 22:37:02 -0500

    Initial commit

---
+++ lib/chronicle/pinboard.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "pinboard/version"
+
+module Chronicle
+  module Pinboard
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
